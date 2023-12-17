5105a17515313bd9c874f4cf8a0291edce9cacf3
sarauzhanovska <uzhanovskasara@gmail.com>
2023-12-17 23:19:42 +0100

    sum of five numbers

---
+++ arrayOfFiveNumbersAndSum/script.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 10, 55, 20];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log(sum);
+alert(`The sum of these numbers is: ${sum}`)
+
+
+
+
